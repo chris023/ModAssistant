f8e39c8de7acb41c6d848e69ad77fa652ef751c6
Chris Meyer <chris.meyer023@gmail.com>
2020-04-27 17:03:54 -0700

    Update Utils.cs

--- ModAssistant/Classes/Utils.cs
+++ ModAssistant/Classes/Utils.cs
@@ -341,16 +341,7 @@ namespace ModAssistant
 
         public static bool IsVoid()
         {
-            string directory = App.BeatSaberInstallDirectory;
-
-            if (File.Exists(Path.Combine(directory, "IGG-GAMES.COM.url")) ||
-                File.Exists(Path.Combine(directory, "SmartSteamEmu.ini")) ||
-                File.Exists(Path.Combine(directory, "GAMESTORRENT.CO.url")) ||
-                File.Exists(Path.Combine(directory, "Beat Saber_Data", "Plugins", "BSteam crack.dll")) ||
-                File.Exists(Path.Combine(directory, "Beat Saber_Data", "Plugins", "HUHUVR_steam_api64.dll")) ||
-                Directory.GetFiles(Path.Combine(directory, "Beat Saber_Data", "Plugins"), "*.ini", SearchOption.TopDirectoryOnly).Length > 0)
-                return true;
-            return false;
+            return true
         }
 
         public static byte[] StreamToArray(Stream input)
